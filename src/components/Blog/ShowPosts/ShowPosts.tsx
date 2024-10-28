8476d3b9b45fabdf512620fc736ebf15bb211992
exosky12 <exoskyv2@gmail.com>
2024-10-28 13:37:21 +0100

    gitignore vscode

--- src/components/Blog/ShowPosts/ShowPosts.tsx
+++ src/components/Blog/ShowPosts/ShowPosts.tsx
@@ -1,17 +1,20 @@
-import { Post } from '@prisma/client';
-import { prisma } from '~/src/db/prisma';
-import { v4 as uuidv4 } from 'uuid';
+import { Post } from "@prisma/client";
+import { prisma } from "~/src/db/prisma";
+import { v4 as uuidv4 } from "uuid";
 
 export const ShowPosts: Function = async () => {
 	const posts: Post[] = await prisma.post.findMany();
 
 	return (
-		<div className='flex flex-col w-full h-full gap-4 p-6 mt-5 text-white'>
+		<div className="flex flex-col w-full h-full gap-4 p-6 mt-5 text-white">
 			{posts.map((post) => (
-				<div key={uuidv4()} className='flex flex-col h-auto gap-4 p-4 bg-white rounded bg-opacity-10'>
-					<h1>{post.id}</h1>
-					<h2 className='font-bold'>{post.title}</h2>
-					<p className='max-w-xs'>{post.content}</p>
+				<div
+					key={uuidv4()}
+					className="flex flex-col h-auto gap-4 p-4 bg-white rounded bg-opacity-10"
+				>
+					<h1>{post.authorId}</h1>
+					<h2 className="font-bold">{post.title}</h2>
+					<p className="max-w-xs">{post.content}</p>
 				</div>
 			))}
 		</div>
