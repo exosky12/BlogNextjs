ecf09c071e256fdb70feced85a25aae7314380d5
exosky12 <102408092+exosky12@users.noreply.github.com>
2024-10-28 13:37:56 +0100

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
 		<nav className='min-w-full p-4 border-b-2 border-white border-solid'>
 			<ul className='flex justify-between text-white'>
 				<Link href='/'>Mon blog</Link>
-				<Link href='/QA'>Questions/ Réponses</Link>
+				<Link href='/'>Questions/ Réponses</Link>
 				<Link href='/Log'>Connexion</Link>
 			</ul>
 		</nav>
